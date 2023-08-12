eb00d08816fcccf9e2db7a2cd9008fbfc277741d
jeensh <jeensh25@gmail.com>
2023-08-12 13:05:12 +0900

    (추가) Order, OrderItem의 비즈니스 로직 및 생성 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,4 +23,23 @@ public class OrderItem {
     private int orderPrice; // 주문 가격(주문 당시의 가격, 상품 가격은 바뀔 수 있음)
     private int count;  // 주문 수량
 
+    //==생성 메서드==//
+    public sealed OrderItem createdOrderItem(Item item, int orderPrice, int count){
+     OrderItem orderItem = new OrderItem();
+     orderItem.setItem(item);
+     orderItem.setOrderPrice(orderPrice);
+     orderItem.setCount(count);
+
+     item.removeStock(count);
+     return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getCount() * getOrderPrice();
+    }
 }
