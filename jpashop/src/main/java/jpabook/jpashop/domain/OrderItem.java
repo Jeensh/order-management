c3e84b462e0899f47eaef67690c5e97dc82bf0c3
jeensh <jeensh25@gmail.com>
2023-08-12 14:10:50 +0900

    (추가) OrderRepository 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,14 +24,14 @@ public class OrderItem {
     private int count;  // 주문 수량
 
     //==생성 메서드==//
-    public sealed OrderItem createdOrderItem(Item item, int orderPrice, int count){
-     OrderItem orderItem = new OrderItem();
-     orderItem.setItem(item);
-     orderItem.setOrderPrice(orderPrice);
-     orderItem.setCount(count);
+    public static OrderItem createdOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
 
-     item.removeStock(count);
-     return orderItem;
+        item.removeStock(count);
+        return orderItem;
     }
 
     //==비즈니스 로직==//
